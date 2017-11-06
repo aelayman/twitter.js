a74f5759b66676e5a18dc2c5298b61896a9c9f79
Ann Layman <annelayman@gmail.com>
2017-11-06 18:29:19 -0500

    Set up start commands in json package

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000!')
+app.use((req, res, next) => {
+    console.log('METHOD: ', req.method)
+    console.log('URI', req.path)
+    next()
   })
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!')
+  })
